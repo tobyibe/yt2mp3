76985b10a22400d91797950b7c6342ff84292729
tobyibe <tobyibe0@gmail.com>
2024-05-18 19:37:51 +0100

    download link over thumbnail image instead of opening new window

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,12 @@
 import React, {useState} from 'react'
-import axios from 'axios'
+
 const Main = () => {
     const [url,setUrl] = useState(""); 
     const [statusMessage,setStatusMessage] = useState("");
     const [thumbNail,setThumbNail] = useState();
+    const [downloadUrl, setDownloadUrl] = useState();
     async function query(e) {
+        e.preventDefault();
         const searchParams = new URLSearchParams(url.split('?')[1]);
         var id = searchParams.get("v");
         if (id === null){
@@ -12,11 +14,10 @@ const Main = () => {
             id = parts[2];
         }
         if (id.length !== 11){
-            console.log("Invalid Link");
             setStatusMessage("Inavlid Link")
             return
         }
-        console.log(id);
+
         const queryUrl = "https://youtube-mp3-download1.p.rapidapi.com/dl?id="+id;
         const options = {
             method: 'GET',
@@ -32,23 +33,27 @@ const Main = () => {
             setStatusMessage("Download Failed");
             return;
         }
-        setStatusMessage ("Title: "+ result.title + " Download Successful")
+        setStatusMessage ("Download Successful. Click Image to download")
         setThumbNail(result.thumb);
-        setTimeout(() => {
-            window.open(result.link, "blank")
-        },1000)
+        setDownloadUrl(result.link);
+        console.log(downloadUrl);
+        console.log(thumbNail);
+
 
     }
   return (
-    <div className=' flex flex-col items-center dark:bg-gray-700 dark:text-gray-300 transition-all p-10 space-y-5 bg-slate-100 border-2 rounded-lg m-5'>
-        <p className=' text-5xl font-bold'> Free YouTube To MP3 Converter and Downloader</p>
+    <div className='sm:text-start text-center  flex flex-col items-center dark:bg-gray-700 dark:text-gray-300 transition-all p-10 space-y-5 bg-slate-100 border-2 rounded-lg m-5'>
+        <p className=' text-2xl sm:text-5xl font-bold'> Free YouTube To MP3 Converter and Downloader</p>
         <p className='text-xl'>Simply Paste the YouTube Video URL below</p>
-        <div className='sm:flex space-y-5 sm:space-y-0 sm:space-x-5 sm:w-full '>
+        <form className='sm:flex space-y-5 sm:space-y-0 sm:space-x-5 sm:w-full '>
             <input placeholder='Enter YouTube Link...' className='rounded shadow-2xl w-full  text-lg text-black' onChange={(event) => setUrl(event.target.value)}></input>
-            <button  className='px-2 py-1 dark:bg-blue-600 dark:hover:bg-blue-800 rounded-lg bg-slate-300 hover:bg-slate-400 transition-all' onClick={query}>Convert</button>            
-        </div>
-        <p className = "">{statusMessage}</p>
-        <img src={thumbNail} className=' w-72 sm:w-1/2 rounded-lg shadow-2xl'></img>
+            <button className='px-2 py-1 dark:bg-blue-600 dark:hover:bg-blue-800 rounded-lg bg-slate-300 hover:bg-slate-400 transition-all' onClick={query}>Convert</button>            
+        </form>
+        <p>{statusMessage}</p>
+        <a href={downloadUrl} className='w-96'> 
+            <img src={thumbNail} className=' w-full rounded-lg shadow-2xl hover:scale-105 transition-all' />
+        </a>
+     
 
     </div>
   )
